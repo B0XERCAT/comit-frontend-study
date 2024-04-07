fdffddba92cf56065fd856cc898d063f13b20f28
b0xercat <seojin3154@naver.com>
2024-04-07 20:55:23 +0900

    feat: add react dom router

---
+++ 7w/src/components/StudyDetail.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StudyDetail() {
+  return <p>StudyDetail</p>;
+}
---
+++ 7w/src/components/StudyList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function StudyList() {
+  return <p>StudyList</p>;
+}
