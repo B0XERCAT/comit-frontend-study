e15104c9883d3050835d5806a6c7e9c30c398eb3
b0xercat <seojin3154@naver.com>
2024-04-08 00:25:31 +0900

    feat: add study detail

--- 7w/src/components/StudyDetail.tsx
+++ 7w/src/components/StudyDetail.tsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router-dom";
+import { data } from "../data";
+
 export default function StudyDetail() {
-  return <p>StudyDetail</p>;
+  const index = Number(useParams().index);
+  const study = data[index];
+  return (
+    <main>
+      <img src={study.imageSrc} alt={study.title} width={300} />
+      <h1>{study.title}</h1>
+      <div id="info-wrapper">
+        <p>📚 {study.level}</p>
+        <p>🏢 {study.campus}</p>
+        <p>👤 {study.mentor}</p>
+        <p>🔧 {study.stack.join(", ")}</p>
+      </div>
+      <p id="description">{study.description}</p>
+    </main>
+  );
 }
