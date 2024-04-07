0bacd69185a860183fbf0c04b05e08e6bea7b1a9
b0xercat <seojin3154@naver.com>
2024-04-07 23:59:34 +0900

    feat: add reset.css, implement study card and list

--- 7w/src/components/StudyList.tsx
+++ 7w/src/components/StudyList.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { data } from "../data";
+import StudyCard from "./StudyCard";
 
 export default function StudyList() {
-  return <p>StudyList</p>;
+  return (
+    <div id="card-wrapper">
+      {data.map((study, index) => {
+        return <StudyCard key={index} {...study} index={index} />;
+      })}
+    </div>
+  );
 }
