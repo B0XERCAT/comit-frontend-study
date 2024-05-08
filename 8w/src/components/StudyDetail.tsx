f8070b41e59da41f1beb41a22c3228932d4d09e5
b0xercat <seojin3154@naver.com>
2024-05-08 14:24:54 +0900

    refactor: change icon with react icons

--- 8w/src/components/StudyDetail.tsx
+++ 8w/src/components/StudyDetail.tsx
@@ -1,5 +1,9 @@
 import { useParams } from "react-router-dom";
 import { data } from "../data";
+import { FaSchoolFlag } from "react-icons/fa6";
+import { IoPersonSharp } from "react-icons/io5";
+import { MdOutlineSignalCellularAlt } from "react-icons/md";
+import { RiStackOverflowLine } from "react-icons/ri";
 
 export default function StudyDetail() {
   const index = Number(useParams().index);
@@ -9,10 +13,18 @@ export default function StudyDetail() {
       <img src={study.imageSrc} alt={study.title} width={300} />
       <h1>{study.title}</h1>
       <div id="info-wrapper">
-        <p>📚 {study.level}</p>
-        <p>🏢 {study.campus}</p>
-        <p>👤 {study.mentor}</p>
-        <p>🔧 {study.stack.join(", ")}</p>
+        <p>
+          <MdOutlineSignalCellularAlt /> {study.level}
+        </p>
+        <p>
+          <FaSchoolFlag /> {study.campus}
+        </p>
+        <p>
+          <IoPersonSharp /> {study.mentor}
+        </p>
+        <p>
+          <RiStackOverflowLine /> {study.stack.join(", ")}
+        </p>
       </div>
       <p id="description">{study.description}</p>
     </main>
