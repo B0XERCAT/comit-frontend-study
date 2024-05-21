46dcc3c9b0b9c502cfb2645d9f04d08a6e46d9cc
b0xercat <seojin3154@naver.com>
2024-05-21 21:48:37 +0900

    feat: scaffold react project with vite

--- 12w/src/components/Badge.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Badge({ children }: { children: React.ReactNode }) {
-  return <div className="badge">{children}</div>;
-}
--- 12w/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Study {
-  imageSrc: string;
-  title: string;
-  mentor: string;
-  level: string;
-  stack: string[];
-  campus: string;
-  description: string;
-}
